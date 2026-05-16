b261c17cfeab0e1aac6686397c797dadd0b64342
Nanasaheb29 <nanabarkul29@gmail.com>
2026-05-16 06:25:46 +0530

    first commit

---
+++ src/com/java/addition_program/adiition_of_two_numbers.java
@@ -0,0 +1,15 @@
+package com.java.addition_program;
+
+public class adiition_of_two_numbers {
+
+	public static void main(String[] args) {
+
+		int num_ = 10;
+		int num_2 = 20;
+		
+		int sum = num_ + num_2;
+		
+		System.out.println(sum);
+	}
+
+}
